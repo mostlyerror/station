00fa9c2d0ccae1fb49bf5b501e2df4b0cf88d1e3
Benjamin Poon <benjamintpoon@gmail.com>
2020-04-29 09:37:23 -0600

    add gitignore and some request logging

--- server.js
+++ server.js
@@ -3,12 +3,24 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-
-
 app.use(bodyParser.json());
 
-app.use('/books', require('./controllers/books'))
+app.use((error, req, res, next) => {
+  console.error(error)
+  console.log(req.url)
+  console.log(req.body)
+  next(error)
+})
+
+
+app.use((req, res, next) => {
+  console.log(req.url)
+  console.log(req.body)
+  next()
+})
+
 app.use('/users', require('./controllers/users'))
+app.use('/books', require('./controllers/books'))
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`listening on ${port}`))
